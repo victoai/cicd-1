f30d54a86f3b80e199dfa3fa44abc2370e0e0cc7
victoai <victoai@naver.com>
2025-12-15 21:26:13 +0900

    fisrt

---
+++ src/main/java/com/example/cicd_1/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.cicd_1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @ResponseBody
+    @GetMapping("/hi")
+    public  String hi(){
+        return  "hi";
+    }
+}
